363281e2fa1d84461522e7742fb6dcd1790d6d5e
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-01-11 14:53:43 +0500

    page  added

---
+++ contact.js
@@ -0,0 +1,42 @@
+const MessageSend = () => {
+    (function(){
+      emailjs.init({
+        publicKey: "Ql5XR0NamqOZmaaX3",
+      });
+   })();
+  
+    let userName = document.getElementById("name").value;
+    let userEmail = document.getElementById("email").value;
+    let userSubject = document.querySelector("#subject").value;
+    let userMessage =document.querySelector("#message").value; 
+  
+    if(!userEmail , !userMessage ,!userName ,!userSubject){
+      alert("PLEASE FILL INPUT");
+      return;
+    }
+  
+  
+    let object = {
+      Full_Name:userName,
+      Email: userEmail,
+      subject: userSubject,
+      Message: userMessage,
+    }
+  
+    emailjs.send("service_e4j4fue" , "template_bjzfhaf" , object)
+    .then((response) => {
+  alert("Sending Complete");
+  console.log(response);
+    })
+    .catch( (error) => {
+  console.log(error);
+    })
+  
+  }
+  
+  const Refresh = () => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value ="";
+    document.querySelector("#message").value = ""; 
+    document.querySelector("#subject").value = "";
+  }
